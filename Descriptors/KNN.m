aa7f9cd6efaccfa1a30bbc2f28543817cc09ed9e
AMESJOUN <Amesjounoutmane@gmail.com>
2022-09-20 09:01:20 +0200

    Add files via upload

---
+++ Descriptors/KNN.m
@@ -0,0 +1,114 @@
+clc
+clear all
+close all
+warning off all
+%%%%%%%%%%%%%%%%%%%%%%%%%%  KNN_LBP18   %%%%%%%%%%%%%%%%
+%  load features_LBP18.mat
+%  label=data.Labels;
+%  C = cellstr(label)
+%   X = features_LBP18';
+%  Y = C;
+%  KNN_LBP18 = fitcknn(X,Y,'NumNeighbors',5,'Standardize',1)
+%  save KNN_LBP18 KNN_LBP18
+
+ 
+%%%%%%%%%%%%%%%%%%%%%%%%%%% KNN_hist  %%%%%%%%%%%%%%%%%
+%  load features_hist.mat
+%  label=data.Labels;
+%  C = cellstr(label)
+%   X = features_hist';
+%  Y = C;
+%  KNN_hist = fitcknn(X,Y,'NumNeighbors',5,'Standardize',1)
+%  save KNN_hist KNN_hist
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%% KNN_cgram  %%%%%%%%%%%%%%%%%
+
+%  load features_cgram.mat
+%  label=data.Labels;
+%  C = cellstr(label)
+%   X = features_cgram';
+%  Y = C;
+%  KNN_cgram = fitcknn(X,Y,'NumNeighbors',5,'Standardize',1)
+%  save KNN_cgram KNN_cgram
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%% KNN_haar  %%%%%%%%%%%%%%%%%
+
+%  load features_haar.mat
+%  label=data.Labels;
+%  C = cellstr(label)
+%   X = features_haar';
+%  Y = C;
+%  KNN_haar = fitcknn(X,Y,'NumNeighbors',5,'Standardize',1)
+%  save KNN_haar KNN_haar
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%% KNN_HAR  %%%%%%%%%%%%%%%%%
+
+%  load features_HAR.mat
+%  label=data.Labels;
+%  C = cellstr(label)
+%   X = features_HAR';
+%  Y = C;
+%  KNN_HAR = fitcknn(X,Y,'NumNeighbors',5,'Standardize',1)
+%  save KNN_HAR KNN_HAR
+ 
+%%%%%%%%%%%%%%%%%%%%%%%%%%% KNN_histMask  %%%%%%%%%%%%%%%%%
+
+%  load features_histMask.mat
+%  label=data.Labels;
+%  C = cellstr(label)
+%   X = features_histMask';
+%  Y = C;
+%  KNN_histMask = fitcknn(X,Y,'NumNeighbors',5,'Standardize',1)
+%  save KNN_histMask KNN_histMask
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%% KNN_gabor  %%%%%%%%%%%%%%%%%
+%  load features_gabor.mat
+%  label=data.Labels;
+%  C = cellstr(label)
+%   X = features_gabor';
+%  Y = C;
+%  KNN_gabor = fitcknn(X,Y,'NumNeighbors',5,'Standardize',1)
+%  save KNN_gabor KNN_gabor
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%% KNN_CM  %%%%%%%%%%%%%%%%%
+
+%  load features_CM.mat
+%  label=data.Labels;
+%  C = cellstr(label)
+%   X = features_CM';
+%  Y = C;
+%  KNN_CM = fitcknn(X,Y,'NumNeighbors',5,'Standardize',1)
+%  save KNN_CM KNN_CM
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%% KNN_HARri  %%%%%%%%%%%%%%%%%
+% 
+%  load features_HARri.mat
+%  label=data.Labels;
+%  C = cellstr(label)
+%   X = features_HARri';
+%  Y = C;
+%  KNN_HARri = fitcknn(X,Y,'NumNeighbors',5,'Standardize',1)
+%  save KNN_HARri KNN_HARri
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%% KNN_GLCM13 %%%%%%%%%%%%%%%%%%%%%%%
+
+% 
+%  load features_GLCM13.mat
+%  label=data.Labels;
+%  C = cellstr(label)
+%   X = features_GLCM13';
+%  Y = C;
+%  KNN_GLCM13 = fitcknn(X,Y,'NumNeighbors',5,'Standardize',1)
+%  save KNN_GLCM13 KNN_GLCM13
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%% KNN_HM %%%%%%%%%%%%%%%%%%%%%%%
+
+%  load features_HM.mat
+%  label=data.Labels;
+%  C = cellstr(label)
+%   X = features_HM';
+%  Y = C;
+%  KNN_HM = fitcknn(X,Y,'NumNeighbors',5,'Standardize',1)
+%  save KNN_HM KNN_HM
